7b9bcd14725fee44c968cb8c95bedff0c0f9fad3
Abhay Kumar <4everabhay@gmail.com>
2025-08-30 18:08:20 +0000

    what i obseve it overlap when i scroll right or left with dashboard ,exe

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -15,7 +15,7 @@ export default function DashboardLayout({
         <DashboardSidebar />
         <SidebarInset>
             <DashboardHeader />
-            <main className="flex-1 p-4 sm:p-6 lg:p-8 bg-background">
+            <main className="flex-1 p-4 sm:p-6 lg:p-8 bg-background overflow-hidden">
             {children}
             </main>
         </SidebarInset>
